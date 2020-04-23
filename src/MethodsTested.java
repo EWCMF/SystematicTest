9386119e94b1292ea0226352338fb8dc954154e5
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-04-23 11:06:30 +0200

    maxValueOf added.

--- src/MethodsTested.java
+++ src/MethodsTested.java
@@ -2,4 +2,8 @@ public class MethodsTested {
     public boolean validateLength(String text) {
         return text.length() >= 1 && text.length() <= 30;
     }
+
+    public int maxValueOf(int a, int b) {
+        return Math.max(a, b);
+    }
 }
--- test/MethodsTestedTest.java
+++ test/MethodsTestedTest.java
@@ -22,9 +22,45 @@ class MethodsTestedTest {
 
     @Test
     void validateLength3() {
-        String input = "qwertyuiopasdfghjklzxcvbnmqwertyu";
+        String input = "qwertyuiopasdfghjklzxcvbnmqwert";
         MethodsTested methodsTested = new MethodsTested();
         boolean output = methodsTested.validateLength(input);
         assertFalse(output);
+    }
+
+    @Test
+    void maxValueOf1() {
+        int a = 1;
+        int b = 2;
+        MethodsTested methodsTested = new MethodsTested();
+        int output = methodsTested.maxValueOf(a, b);
+        assertEquals(b, output);
+    }
+
+    @Test
+    void maxValueOf2() {
+        int a = 2;
+        int b = 1;
+        MethodsTested methodsTested = new MethodsTested();
+        int output = methodsTested.maxValueOf(a, b);
+        assertEquals(a, output);
+    }
+
+    @Test
+    void maxValueOf3() {
+        int a = 1;
+        int b = 1;
+        MethodsTested methodsTested = new MethodsTested();
+        int output = methodsTested.maxValueOf(a, b);
+        assertEquals(a, output);
+    }
+
+    @Test
+    void maxValueOf4() {
+        int a = 1;
+        int b = 1;
+        MethodsTested methodsTested = new MethodsTested();
+        int output = methodsTested.maxValueOf(a, b);
+        assertEquals(b, output);
     }
 }
